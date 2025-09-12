9be413a11b268f1719666efce0338edd482fb8a6
rajeshinnovatics7978 <rajesh@teaminnovatics.com>
2025-09-12 19:57:32 +0530

    base

--- nicegui_app/layout/navbar.py
+++
@@ -1,8 +0,0 @@
-from nicegui import ui
-
-def navbar():
-    with ui.header().classes('bg-black text-white'):
-        ui.label('NiceGUI').classes('text-xl font-bold ml-4')
-        with ui.row().classes('ml-auto mr-4'):
-            ui.link('Home', '/').classes('text-white hover:underline')
-            ui.link('Demo', '/demo').classes('text-white hover:underline')
--- nicegui_app/main.py
+++
@@ -1,5 +0,0 @@
-from nicegui import ui
-from pages import home, demo
-
-
-ui.run()
